0ed35a5bc5a2ff060ad44966979b60e0ac844b72
Nat Welch <nat@natwelch.com>
2023-02-25 03:03:45 +0000

    books

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -6,3 +6,8 @@ type Instance struct {
 func New() (*Instance, error) {
 	return nil, nil
 }
+
+func (s *Instance) GenerateSchedule(ctx context.Context, past []*Entry, length time.Duration, count int64) ([]*Entry, error) {
+
+	return nil, nil
+}
